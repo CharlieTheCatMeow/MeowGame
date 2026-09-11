59bc82407167a108730268959fe18f3f6265602f
Xuan Zhu <charliecheesycatmeow@gmail.com>
2026-09-11 14:57:30 +0200

    Added sounds

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var master_bus_index: int = AudioServer.get_bus_index("Master")
+@onready var clickSound: AudioStreamPlayer = $ClickSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,14 +16,23 @@ func _process(delta: float) -> void:
 
 
 func _on_begin_pressed() -> void:
+	clickSound.stop()
+	clickSound.play()
+	await await get_tree().create_timer(0.03, false).timeout
 	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
 	Global.lives = 5
 	Global.minigames_done = 0
 
 
 func _on_quit_pressed() -> void:
+	clickSound.stop()
+	clickSound.play()
+	await await get_tree().create_timer(0.03, false).timeout
 	get_tree().quit()
 
 
 func _on_settings_pressed() -> void:
+	clickSound.stop()
+	clickSound.play()
+	await await get_tree().create_timer(0.03, false).timeout
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
